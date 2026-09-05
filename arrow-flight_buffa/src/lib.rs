7c8d856f63408ea74294e60a481d257911359264
Egor Markov <mark_ee@live.com>
2026-09-05 13:01:01 +0300

    wip

--- arrow-flight_buffa/src/lib.rs
+++ arrow-flight_buffa/src/lib.rs
@@ -1,7 +1,1 @@
-// This code is generated so we don't want to fix any lint violations manually
-#[allow(clippy::allow_attributes)]
-#[allow(clippy::all)]
-mod r#gen {
-    #![allow(missing_docs)]
-    include!("arrow.flight.protocol.rs");
-}
+mod sql;
--- arrow-flight_buffa/src/sql/mod.rs
+++ arrow-flight_buffa/src/sql/mod.rs
@@ -1,1 +1,44 @@
+use buffa_types::Any;
 
+// This code is generated so we don't want to fix any lint violations manually.
+#[allow(clippy::allow_attributes)]
+#[allow(clippy::all)]
+mod generated {
+    #![allow(missing_docs)]
+    include!("arrow.flight.protocol.sql.rs");
+}
+
+pub use generated::{CommandStatementQuery, TicketStatementQuery};
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_type_url() {
+        assert_eq!(
+            TicketStatementQuery::TYPE_URL,
+            "type.googleapis.com/arrow.flight.protocol.sql.TicketStatementQuery"
+        );
+        assert_eq!(
+            CommandStatementQuery::TYPE_URL,
+            "type.googleapis.com/arrow.flight.protocol.sql.CommandStatementQuery"
+        );
+    }
+
+    #[test]
+    fn test_buffa_any_pack_unpack() {
+        let query = CommandStatementQuery {
+            query: "select 1".to_string(),
+            transaction_id: None,
+        };
+        let any = Any::pack(&query, CommandStatementQuery::TYPE_URL);
+        assert!(any.is_type(CommandStatementQuery::TYPE_URL));
+        let unpack_query = any
+            .unpack_if(CommandStatementQuery::TYPE_URL)
+            .unwrap()
+            .unwrap();
+        assert_eq!(query, unpack_query);
+    }
+}
