0196bcf9c1aa94eb7efa881187452c92865d2e50
Egor Markov <mark_ee@live.com>
2026-09-05 12:25:55 +0300

    wip

--- arrow-flight_buffa/src/lib.rs
+++ arrow-flight_buffa/src/lib.rs
@@ -1,1 +1,7 @@
-
+// This code is generated so we don't want to fix any lint violations manually
+#[allow(clippy::allow_attributes)]
+#[allow(clippy::all)]
+mod r#gen {
+    #![allow(missing_docs)]
+    include!("arrow.flight.protocol.rs");
+}
